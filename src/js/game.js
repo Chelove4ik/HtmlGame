a887a82532af413555682991f2ab8ebc9b56e46a
Denis <dennis220802@gmail.com>
2021-04-28 16:15:21 +1000

    Добавлен фон игры и сделан макет основных функций

--- src/js/game.js
+++ src/js/game.js
@@ -1,2 +1,24 @@
 let canvas = document.getElementById('game');
 let context = canvas.getContext('2d');
+
+let imgFon = new Image();
+imgFon.src = 'src/image/background.jpg';
+
+imgFon.onload = () => {
+    game();
+}
+
+// Основной игровой цикл
+function game () {
+    update();
+    render();
+    requestAnimationFrame(game);
+}
+
+function update() {
+
+}
+
+function render() {
+    context.drawImage(imgFon, 0, 0, canvas.clientWidth, canvas.clientHeight);
+}
